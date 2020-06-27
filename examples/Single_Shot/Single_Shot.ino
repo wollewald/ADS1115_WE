5a579553c91aa902ded81482df4bee3666b8e176
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2020-06-27 15:22:34 +0200

    Update Single_Shot.ino

--- examples/Single_Shot/Single_Shot.ino
+++ examples/Single_Shot/Single_Shot.ino
@@ -52,7 +52,7 @@ void setup() {
    *  
    *  ADS1115_ASSERT_AFTER_1  -> after 1 conversion
    *  ADS1115_ASSERT_AFTER_2  -> after 2 conversions
-   *  ADS1115_ASSERT_AFTER_3  -> after 3 conversions
+   *  ADS1115_ASSERT_AFTER_4  -> after 4 conversions
    *  ADS1115_DISABLE_ALERT   -> disable comparator / alert pin (default) 
    */
   //adc.setAlertPinMode(ADS1115_ASSERT_AFTER_1); //uncomment if you want to change the default
