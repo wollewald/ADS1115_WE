4cb338458cf49e4f2e76b603cad26dfe3adca78b
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2020-07-25 21:36:10 +0200

    Update Single_Shot_Conv_Ready_Controlled.ino

--- examples/Single_Shot_Conv_Ready_Controlled/Single_Shot_Conv_Ready_Controlled.ino
+++ examples/Single_Shot_Conv_Ready_Controlled/Single_Shot_Conv_Ready_Controlled.ino
@@ -14,7 +14,7 @@
 #define I2C_ADDRESS 0x48
 
 ADS1115_WE adc(I2C_ADDRESS);
-// ADS1115_WE adc = ADS1115_WE(); // Alternative: sets default address 0x48
+// ADS1115_WE adc = ADS1115_WE(); // Alternative: uses default address 0x48
 
 void setup() {
   Wire.begin();
@@ -33,7 +33,7 @@ void setup() {
    * ADS1115_RANGE_0512  ->  +/- 512 mV
    * ADS1115_RANGE_0256  ->  +/- 256 mV
    */
-  adc.setVoltageRange_mV(ADS1115_RANGE_6144); //comment line/change paramater to change range
+  adc.setVoltageRange_mV(ADS1115_RANGE_6144); //comment line/change parameter to change range
 
   /* Set the inputs to be compared
    *  
@@ -46,7 +46,7 @@ void setup() {
    *  ADS1115_COMP_2_GND  ->  compares 2 with GND
    *  ADS1115_COMP_3_GND  ->  compares 3 with GND
    */
-  adc.setCompareChannels(ADS1115_COMP_0_GND); //comment line/change paramater to change channels
+  adc.setCompareChannels(ADS1115_COMP_0_GND); //comment line/change parameter to change channels
 
   /* Set number of conversions after which the alert pin will be active
    * - or you can disable the alert 
@@ -70,7 +70,7 @@ void setup() {
    *  ADS1115_475_SPS 
    *  ADS1115_860_SPS 
    */
-  adc.setConvRate(ADS1115_8_SPS); //comment line/change paramater to change SPS
+  adc.setConvRate(ADS1115_8_SPS); //comment line/change parameter to change SPS
 
   /* Set continous or single shot mode:
    * 
