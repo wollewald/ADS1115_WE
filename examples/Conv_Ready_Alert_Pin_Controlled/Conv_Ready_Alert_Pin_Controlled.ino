e5d0c4189b5b57b4a1ecdc7891781dabe1bccf13
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2026-03-27 15:53:25 +0100

    Change serial baud rate to 115200
    Updated serial baud rate and improved comments.

--- examples/Conv_Ready_Alert_Pin_Controlled/Conv_Ready_Alert_Pin_Controlled.ino
+++ examples/Conv_Ready_Alert_Pin_Controlled/Conv_Ready_Alert_Pin_Controlled.ino
@@ -28,7 +28,7 @@ ADS1115_WE adc = ADS1115_WE(I2C_ADDRESS);
 
 void setup() {
   Wire.begin();
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(interruptPin, INPUT_PULLUP);
   if(!adc.init()){
     Serial.println("ADS1115 not connected!");
